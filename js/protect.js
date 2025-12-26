6363f28e2ba327882f1821cd2b9ee7c9ce5a72d2
Hexamora <hexa1868@gmail.com>
2025-12-26 18:05:54 +0700

    new

--- js/protect.js
+++ js/protect.js
@@ -3,20 +3,31 @@ const allowedKey = "OX48I-9x71A";
 const params = new URLSearchParams(window.location.search);
 const key = params.get("key");
 
-if (key === allowedKey) {
-    sessionStorage.setItem("access", "granted");
-}
+const allow = () => {
+    document.documentElement.style.display = "block";
+};
 
-if (sessionStorage.getItem("access") !== "granted") {
+const deny = () => {
     document.documentElement.innerHTML = `
     <head>
         <title>403 Forbidden</title>
     </head>
-    <body style="font-family:sans-serif;text-align:center;padding:50px">
+    <body style="margin:0;display:flex;justify-content:center;align-items:center;height:100vh;font-family:sans-serif;background:#0f172a;color:#fff">
+        <div>
         <h1>403 Forbidden</h1>
         <p>Access denied.</p>
+        </div>
     </body>
     `;
-    throw new Error("Access blocked");
+    throw new Error("Blocked");
+};
+
+if (key === allowedKey) {
+    sessionStorage.setItem("access", "granted");
+    allow();
+} else if (sessionStorage.getItem("access") === "granted") {
+    allow();
+} else {
+    deny();
 }
 })();
