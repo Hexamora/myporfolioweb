85d8c1801cd8212e82760a2d4766356a2e4f61eb
Hexamora <hexa1868@gmail.com>
2025-12-26 18:24:32 +0700

    new

--- js/secure.js
+++ js/secure.js
@@ -1,7 +1,7 @@
 if (sessionStorage.getItem("access") !== "granted") {
-  throw new Error("Blocked");
+    throw new Error("Blocked");
 }
 
 document.addEventListener("contextmenu", e => {
-  if (e.target.tagName === "IMG") e.preventDefault();
+    if (e.target.tagName === "IMG") e.preventDefault();
 });
